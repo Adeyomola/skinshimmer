be3c43ed7d924b55a56ec0b89f7234a6d80a5bda
Adeyomola <kayzeel@yahoo.com>
2024-10-12 18:08:03 +0100

    sitemap

--- this/app.py
+++ this/app.py
@@ -39,7 +39,7 @@ def create_app():
         session.clear()
         return redirect('/')
     
-    @app.route('/sitemap')
+    @app.route('/sitemap.xml')
     def sitemap():
           return send_from_directory(app.static_folder, 'sitemap.xml')
     
