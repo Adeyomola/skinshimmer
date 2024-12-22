c1ba4b9b94f2d78ff1d828fd2e763cca646045d2
Adeyomola <kayzeel@yahoo.com>
2024-12-22 22:04:00 +0100

    removed SEND_FILE_MAX_AGE_DEFAULT

--- this/app.py
+++ this/app.py
@@ -17,7 +17,6 @@ def create_app():
 
     app.config.from_mapping(
         SECRET_KEY=secret_key,
-        SEND_FILE_MAX_AGE_DEFAULT = 600,
         ENGINE= create_engine(f"mysql://{db_user}:{db_password}@{host}/{db_name}")
     )
 
