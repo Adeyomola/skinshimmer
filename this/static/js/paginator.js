cce64e839a7694eccddd640f783ab8f42c6dfad7
Adeyomola <kayzeel@yahoo.com>
2025-02-07 21:30:28 +0100

    updated paginator.js

--- this/static/js/paginator.js
+++ this/static/js/paginator.js
@@ -2,7 +2,11 @@ const pageNumbers = document.querySelectorAll(".pageNumbers");
 const href = window.location.pathname + window.location.search;
 
 pageNumbers.forEach((page) => {
-  if (href === page.getAttribute("href")) page.className += " active";
-  else if (href === window.location.pathname)
+  if (href === page.getAttribute("href")) {
+    page.className += " active";
+    console.log(href);
+  } else if (href === window.location.pathname) {
     pageNumbers[0].className += " active";
+    console.log(href);
+  }
 });
